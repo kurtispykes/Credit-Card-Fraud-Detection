546ae0d904d4a7ea42a006ee7b56dcd3f2b0c2a1
kurtispykes <kurtispykes@gmail.com>
2020-10-27 13:07:27 +0000

    kaggle dataset

--- packages/scripts/fetch_kaggle_dataset.sh
+++ packages/scripts/fetch_kaggle_dataset.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-kaggle competitions download -c ieee-fraud-detection -p packages/random_forest_model/random_forest_model/input/
+kaggle competitions download -c ieee-fraud-detection -p packages/random_forest_model/random_forest_model/input/ 

d87c89b28a94dffdc3aceeb002440995749bdf5e
kurtispykes <kurtispykes@gmail.com>
2020-10-28 09:32:15 +0000

    Adjusted Storage Path

--- scripts/fetch_kaggle_dataset.sh
+++ scripts/fetch_kaggle_dataset.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-wget "https://www.dropbox.com/s/a0jd4xlsqdloa1k/test_df.csv?" -O test_df.csv t-P packages/random_forest_model/random_forest_model/input/
-wget "https://www.dropbox.com/s/pcf72af8ywisai6/train_folds.csv?dl=0" -O train_folds.csv -P packages/random_forest_model/random_forest_model/input/
+wget "https://www.dropbox.com/s/a0jd4xlsqdloa1k/test_df.csv?" -O test_df.csv t-P random_forest_model/random_forest_model/input/
+wget "https://www.dropbox.com/s/pcf72af8ywisai6/train_folds.csv?dl=0" -O train_folds.csv -P random_forest_model/random_forest_model/input/

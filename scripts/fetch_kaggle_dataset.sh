2fdd1ae24bb2c1fef2cd06ed3ef7c65eb688cbe1
kurtispykes <kurtispykes@gmail.com>
2020-10-28 09:59:57 +0000

    Added full CircleCI path to store data

--- scripts/fetch_kaggle_dataset.sh
+++ scripts/fetch_kaggle_dataset.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-wget "https://www.dropbox.com/s/a0jd4xlsqdloa1k/test_df.csv?" -O random_forest_model/random_forest_model/input/test_df.csv
-wget "https://www.dropbox.com/s/pcf72af8ywisai6/train_folds.csv?dl=0" -O packages/random_forest_model/random_forest_model/input/train_folds.csv
+wget "https://www.dropbox.com/s/a0jd4xlsqdloa1k/test_df.csv?" -O /home/circleci/project/packages/random_forest_model/random_forest_model/input/test_df.csv
+wget "https://www.dropbox.com/s/pcf72af8ywisai6/train_folds.csv?dl=0" -O /home/circleci/project/packages/random_forest_model/random_forest_model/input/test_df.csv

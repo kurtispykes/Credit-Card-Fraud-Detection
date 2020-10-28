cf31c3f3a04110806ebdadca05d81f6348c6f21d
kurtispykes <kurtispykes@gmail.com>
2020-10-28 12:55:50 +0000

    No longer downloading train data

--- scripts/fetch_kaggle_dataset.sh
+++ scripts/fetch_kaggle_dataset.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
 wget "https://www.dropbox.com/s/a0jd4xlsqdloa1k/test_df.csv?" -O /home/circleci/project/packages/random_forest_model/random_forest_model/input/test_df.csv
-wget "https://www.dropbox.com/s/pcf72af8ywisai6/train_folds.csv?dl=0" -O /home/circleci/project/packages/random_forest_model/random_forest_model/input/test_df.csv

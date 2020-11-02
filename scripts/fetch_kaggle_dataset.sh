b716de6ff00e0ab7831aa08346b9155b55f324dc
kurtispykes <kurtispykes@gmail.com>
2020-11-02 09:46:47 +0000

    modified

--- scripts/fetch_kaggle_dataset.sh
+++ scripts/fetch_kaggle_dataset.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-wget "https://www.dropbox.com/s/a0jd4xlsqdloa1k/test_df.csv?" -O /home/circleci/project/packages/random_forest_model/random_forest_model/input/test_df.csv
+wget "https://www.dropbox.com/s/a0jd4xlsqdloa1k/test_df.csv?" -P ../packages/random_forest_model/random_forest_model/input/test_df.csv
